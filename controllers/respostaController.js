884b1ffea842de90c3d435368dd02112ecb92311
micapareddes <micapareddes@gmail.com>
2024-09-28 16:44:48 -0300

    Logs de depuração

--- controllers/respostaController.js
+++ controllers/respostaController.js
@@ -19,7 +19,10 @@ class RespostaController {
         const totalDeTentativasDoQuiz = Number(quiz.tentativas)
         const tentativasAluno = await ModeloResposta.countDocuments({ aluno_id: alunoId, quiz_id })
         if (totalDeTentativasDoQuiz !== 0 && tentativasAluno >= totalDeTentativasDoQuiz) throw new ServidorError(ANSWER_ERROR.NO_MORE_ATTEMPTS)
-
+        console.log('Quiz: ',quiz);
+        console.log('Tentativas quiz convertidas em num: ',totalDeTentativasDoQuiz);
+        console.log('Aluno: ',tentativasAluno);
+        
         const perguntasQuiz = await ModeloResposta.findById(respostaId, 'perguntas_quiz -_id')
         const perguntas = perguntasQuiz.perguntas_quiz
         
